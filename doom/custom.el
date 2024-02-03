048170e01cf6d4db138a8f5a6628c66064455d6f
Joker Hacker <jokerhacker.6521@protonmail.com>
2024-02-03 20:19:13 +0530

    add files

---
+++ doom/custom.el
@@ -0,0 +1,20 @@
+(put 'downcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("013728cb445c73763d13e39c0e3fd52c06eefe3fbd173a766bfd29c6d040f100" "4b6cc3b60871e2f4f9a026a5c86df27905fb1b0e96277ff18a76a39ca53b82e1" "75b2a02e1e0313742f548d43003fcdc45106553af7283fb5fad74359e07fe0e2" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "3cdd0a96236a9db4e903c01cb45c0c111eb1492313a65790adb894f9f1a33b2d" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "570263442ce6735821600ec74a9b032bc5512ed4539faf61168f2fdf747e0668" "ce4234c32262924c1d2f43e6b61312634938777071f1129c7cde3ebd4a3028da" "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "1cae4424345f7fe5225724301ef1a793e610ae5a4e23c023076dc334a9eb940a" "afa47084cb0beb684281f480aa84dab7c9170b084423c7f87ba755b15f6776ef" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files nil)
+ '(package-selected-packages
+   '(lsp-mode lsp-ui clipetty org-superstar ox-hugo elpy minimap)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-themes 'disabled nil)
+(put 'customize-group 'disabled nil)
